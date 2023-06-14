f6b84f76b6297c1436d4371f9d4aa178263f5223
Hector Ruiz <hectorruizlaph@gmail.com>
2023-06-14 11:03:05 -0600

    add runpodId

--- prisma/migrations/20230614165650_runpod_id/migration.sql
+++ prisma/migrations/20230614165650_runpod_id/migration.sql
@@ -1,11 +1,9 @@
 -- CreateTable
 CREATE TABLE "Video" (
     "id" SERIAL NOT NULL,
-    "videoId" TEXT NOT NULL,
+    "videoId" TEXT,
+    "runpodId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Video_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Video_videoId_key" ON "Video"("videoId");
