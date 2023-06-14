368b865e11b5f02612d21e407430f394c37bd8a0
Hector Ruiz <hectorruizlaph@gmail.com>
2023-06-13 19:22:19 -0600

    s3 video

--- pages/api/allvideos.js
+++ pages/api/allvideos.js
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
     const s3 = new AWS.S3({
       accessKeyId: process.env.S3_ACCESS_ID,
       secretAccessKey: process.env.S3_ACCESS_SECRET,
-      endpoint: process.env.S3_ENDPOINT_URL,
       region: process.env.S3_REGION,
     })
 
