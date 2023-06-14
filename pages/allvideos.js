5c1d02b9b4069b7e9150414b41b0f96b1a6dba63
Hector Ruiz <hectorruizlaph@gmail.com>
2023-06-13 19:43:24 -0600

    allvideos

--- pages/allvideos.js
+++ pages/allvideos.js
@@ -8,7 +8,18 @@ export default function AllVideos() {
     const fetchVideos = async () => {
       try {
         const response = await axios.get("/api/allvideos")
-        setVideoUrls(response.data.videoUrls)
+        const videoUrls = response.data.videoUrls
+        setVideoUrls(videoUrls)
+
+        // Fetch the final video ID for each video after a delay
+        const videoIds = await Promise.all(
+          videoUrls.map(async (videoUrl) => {
+            const videoId = await getFinalVideoId(videoUrl)
+            return videoId
+          })
+        )
+
+        console.log("Final Video IDs:", videoIds)
       } catch (error) {
         console.error("Failed to fetch videos:", error)
       }
@@ -16,6 +27,33 @@ export default function AllVideos() {
 
     fetchVideos()
   }, [])
+
+  const getFinalVideoId = async (videoUrl) => {
+    const statusUrl = `https://api.runpod.ai/v2/bbjho7b2sbjsdr/status/${getVideoIdFromUrl(
+      videoUrl
+    )}`
+
+    let videoId = null
+
+    while (!videoId) {
+      const response = await axios.get(statusUrl)
+      videoId = response.data.videoId
+
+      // Wait for 4 seconds before checking the status again
+      await sleep(4000)
+    }
+
+    return videoId
+  }
+
+  const getVideoIdFromUrl = (videoUrl) => {
+    const parts = videoUrl.split("/")
+    return parts[parts.length - 1].split("?")[0]
+  }
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
 
   return (
     <div>
