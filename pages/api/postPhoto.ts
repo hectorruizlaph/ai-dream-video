c20068f49ae2fdc2a6bb32696c5b55f8e06719e0
Hector Ruiz <hectorruizlaph@gmail.com>
2023-07-26 19:14:18 -0600

    test 1 last frame

--- pages/api/postPhoto.ts
+++ pages/api/postPhoto.ts
@@ -1,27 +1,27 @@
-import {NextApiRequest, NextApiResponse} from "next"
-import {getErrorMessage} from "../../utils/getErrorMessage"
-import S3 from "aws-sdk/clients/s3"
-import {randomUUID} from "crypto"
+import {NextApiRequest, NextApiResponse} from 'next'
+import {getErrorMessage} from '../../utils/getErrorMessage'
+import S3 from 'aws-sdk/clients/s3'
+import {randomUUID} from 'crypto'
 
 const s3 = new S3({
-  apiVersion: "2006-03-01",
+  apiVersion: '2006-03-01',
   accessKeyId: process.env.S3_UPLOAD_KEY,
   secretAccessKey: process.env.S3_UPLOAD_SECRET,
   region: process.env.S3_UPLOAD_REGION,
-  signatureVersion: "v4",
+  signatureVersion: 'v4',
 })
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const {query} = req
   const {filename, contentType} = query
 
-  console.log("postPhoto - filename:", filename) // newFile.png
-  console.log("postPhoto - contentType:", contentType) // image/png
+  // console.log("postPhoto - filename:", filename) // newFile.png
+  // console.log("postPhoto - contentType:", contentType) // image/png
 
   try {
     // const client = new S3Client({region: process.env.S3_UPLOAD_REGION})
 
-    const ex = (contentType as string).split("/")[1]
+    const ex = (contentType as string).split('/')[1]
     const Key = `images/${randomUUID()}.${ex}`
 
     const s3Params = {
@@ -31,11 +31,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       ContentType: `image/${ex}`,
     }
 
-    const uploadUrl = await s3.getSignedUrl("putObject", s3Params)
-    console.log("uploadUrl", uploadUrl)
-    const cleanUrl = (uploadUrl as string).split("?")[0]
+    const uploadUrl = await s3.getSignedUrl('putObject', s3Params)
+    // console.log("uploadUrl", uploadUrl)
+    const cleanUrl = (uploadUrl as string).split('?')[0]
 
-    console.log("cleanUrl: ", cleanUrl)
+    // console.log("cleanUrl: ", cleanUrl)
     // const {url, fields} = await createPresignedPost(client, {
     //   Bucket: process.env.S3_UPLOAD_BUCKET!,
     //   Key: fileKey,
@@ -47,13 +47,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     //   Expires: 600,
     // })
 
-    const imageUrl = `https://${process.env.S3_UPLOAD_BUCKET}s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com/${Key}`
+    // const imageUrl = `https://${process.env.S3_UPLOAD_BUCKET}.s3.${process.env.S3_UPLOAD_REGION}.amazonaws.com/${Key}`
 
-    console.log("cleanUrl: ", cleanUrl, "imageUrl:", imageUrl)
+    // console.log("cleanUrl: ", cleanUrl)
 
-    return res.json({cleanUrl, imageUrl})
+    return res.json({cleanUrl})
   } catch (error) {
-    console.log("Error:", error)
+    console.log('Error:', error)
     return res.json({error: getErrorMessage(error)})
   }
 }
