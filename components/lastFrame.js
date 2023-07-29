98012ab71902ef1efc8ff75ea8b07f062183e2e1
Hector Ruiz <hectorruizlaph@gmail.com>
2023-07-28 19:56:33 -0600

    test 2

--- components/lastFrame.js
+++ components/lastFrame.js
@@ -15,19 +15,28 @@ const LastFrame = ({videoUrl}) => {
       let ctx = canvas.getContext('2d')
       ctx.drawImage(this, 0, 0)
       canvas.toBlob(function (blob) {
+        // First, get the signed URL from your endpoint
+        let filename = 'last_frame.png' // Provide an appropriate filename
+        let contentType = 'image/png'
         axios
-          .post('/api/postPhoto', blob, {
-            headers: {
-              'Content-Type': 'image/png',
-            },
-          })
+          .get(`/api/postPhoto?filename=${filename}&contentType=${contentType}`)
           .then((response) => {
-            console.log(response)
-            // Handle the response as needed...
+            // Then, use the signed URL to upload the image data to S3
+            axios
+              .put(response.data.cleanUrl, blob, {
+                headers: {
+                  'Content-Type': contentType,
+                },
+              })
+              .then(() => {
+                console.log('Image uploaded successfully')
+              })
+              .catch((error) => {
+                console.error('Error uploading the image:', error)
+              })
           })
           .catch((error) => {
-            console.error(error)
-            // Handle the error as needed...
+            console.error('Error getting the signed URL:', error)
           })
       }, 'image/png')
     })
