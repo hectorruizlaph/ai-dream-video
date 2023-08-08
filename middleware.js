353f214b5d4293b647f87bd919e75f0c84b184a7
Hector <hectorruizlaph@gmail.com>
2023-08-08 17:29:05 -0600

    3

--- middleware.js
+++ middleware.js
@@ -11,15 +11,15 @@ export function middleware(req) {
   const origin = req.headers.get('origin')
   console.log('origin: ', origin)
 
-  if (origin && !allowedOrigens.includes(origin)) {
-    return new NextResponse(null, {
-      status: 400,
-      statusText: 'Bad Request',
-      headers: {
-        'Content-Type': 'text/plain',
-      },
-    })
-  }
-  const res = NextResponse.next()
+  const res = new NextResponse(null, {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': origin || '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version',
+      'Access-Control-Max-Age': '86400',
+    },
+  })
   return res
 }
