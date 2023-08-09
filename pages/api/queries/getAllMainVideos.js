ce1475fa49b1ebb106e602c7c8416d1df902bcaa
Hector <hectorruizlaph@gmail.com>
2023-08-08 20:41:34 -0600

    remove return

--- pages/api/queries/getAllMainVideos.js
+++ pages/api/queries/getAllMainVideos.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     const allMainVideos = await fetchAllMainVideos()
     // res.setHeader('Access-Control-Allow-Origin', '*')
 
-    return res.status(200).json({
+    res.status(200).json({
       data: {
         allMainVideos,
       },
